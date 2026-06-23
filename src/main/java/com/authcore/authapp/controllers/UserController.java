a6d1aa31c1ccd9a13e3b42534bd8ea0232f5935a
dteso <dtesodev@gmail.com>
2026-06-23 11:52:59 +0200

    changes for clients generation

--- src/main/java/com/authcore/authapp/controllers/UserController.java
+++ src/main/java/com/authcore/authapp/controllers/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/user")
+@RequestMapping(value = "/api/v1/user", produces = "application/json")
 @RequiredArgsConstructor
 @Slf4j
 public class UserController {
